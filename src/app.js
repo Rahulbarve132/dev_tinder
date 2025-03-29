862c9693e2c21a20cd982498d63090b185ffffbb
Rahul Barve <rahulbarve132@gmail.com>
2025-03-29 19:48:18 +0530

    exploring get, post , patch , delete apis

--- src/app.js
+++ src/app.js
@@ -2,21 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user", (req , res)=>{
+    res.send({user_name : "rahul", age : 21})
+})
+
+app.post("/user", (req, res)=>{
+    res.send ("user created")
+})
+
+app.patch("/user", (req, res)=>{
+    res.send ("user updated")
+})
+
+app.delete("/user", (req, res)=>{
+    res.send ("user deleted")
+})
+
 
 app.use("/test",(req, res)=>{
     res.send("hello form test route")
 })
-app.use("/home",(req, res)=>{
-    res.send("hello form home route")
-})
 
-app.use("/khali",(req, res)=>{
-    res.send("hello form khali route")
-})
-
-app.use("/",(req, res)=>{
-    res.send("hello form no ")
-})
 
 
 app.listen(7777 ,()=>{
