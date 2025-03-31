f66554356a7e9f4fa07a750e8c1e3ebea024934a
Rahul Barve <rahulbarve132@gmail.com>
2025-03-31 16:30:52 +0530

    playing with the multiple routes handler

--- src/app.js
+++ src/app.js
@@ -2,36 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user/:id",(req , res)=>{ 
-    console.log(req.params);
-    res.send("hello user");
+app.use("/user", (req, res ,next)=>{
+    console.log("Middleware 1");
+    next();
+},
+(req, res ,next)=>{
+    console.log("Middleware 2");
+    next();
+},
+(req, res ,next)=>{
+    console.log("Middleware 3");
+    res.send("hello form hero 45")
 })
-
-app.get("/user",(req , res)=>{ 
-    console.log(req.query);
-    res.send("hello user query");
-})
-
-app.get("/ab?c",(req, res)=>{
-    console.log({name:"he", age: 20});
-    res.send("hello ab?c");
-})
-
-app.get("/ab+c", (req, res)=>{
-    res.send("working")
-})
-
-app.get("/ab*c", (req, res)=>{
-    res.send("hero hum")
-})
-
-app.get("/*fly/:id",(req, res)=>{
-    console.log(req.params)
-    res.send("hello fly")
-})
-
-
-
 
 
 app.listen(7777 ,()=>{
