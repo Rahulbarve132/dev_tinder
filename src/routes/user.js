994ef7a6a9263e7441379acb15aa2398f5852e4a
Rahul Barve <rahulbarve132@gmail.com>
2025-11-05 17:24:39 +0530

    created feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const userRouter = express.Router();
 const userAuth = require("../middlewares/auth.js");
 const connectionRequest = require("../models/connectionRequest.js");
+const User = require("../models/user.js");
 
 const USER_SAFE_DATA = 'firstName lastName age gender photoUrl bio skills'
+
 
 userRouter.get("/user/request/panding" , userAuth, async (req , res)=>{
     try {
@@ -49,6 +51,47 @@ userRouter.get("/user/connections" , userAuth, async (req , res)=>{
             message : "Connections fetched successfully",
             data : data
         });
+        
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+})
+
+userRouter.get("/feed", userAuth , async(req, res)=>{
+    try {
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        if(limit > 50) limit = 50;
+        const skip = (page - 1) * limit;
+
+        const coneectionRequests = await connectionRequest.find({
+            $or:[
+                {fromUserId : loggedInUser._id},
+                {toUserId : loggedInUser._id}
+            ]
+        }).select('fromUserId toUserId');
+
+        const hideUserFromFeed = new Set();
+        coneectionRequests.forEach((row)=>{
+            hideUserFromFeed.add(row.fromUserId.toString());
+            hideUserFromFeed.add(row.toUserId.toString());
+        });
+
+        const feedUsers = await User.find({
+            $and: [
+                   { _id : {$nin : Array.from(hideUserFromFeed)},},
+                   { _id : {$ne : loggedInUser._id} },
+            ]   
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        
+        res.status(200).json({
+            message : "Feed fetched successfully",
+            data : feedUsers
+        });
+
+        
         
     } catch (err) {
         res.status(400).send("ERROR: " + err.message);
